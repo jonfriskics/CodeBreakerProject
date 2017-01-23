76f15bb5d78ececc655b49f8b91e54ba3c67c597
Jon Friskics <jonfriskics@gmail.com>
2017-01-23 13:53:51 -0500

    first attempt at a solution

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,9 +1,96 @@
-let answer = document.getElementById('answer');
-let attempt = document.getElementById('attempt');
+var answer = document.getElementById('answer');
+var attempt = document.getElementById('attempt');
 
 function guess() {
-    let input = document.getElementById('user-guess');
-    //add functionality to guess function here
+    var input = document.getElementById('user-guess');
+    if(answer.value == "" || attempt.value == "") {
+        setHiddenFields();
+    }
+
+    //Ensure user input is valid, iterate attempt if valid
+    if(!validateInput(input.value)){
+        return;
+    } else {
+        attempt.value++;
+    }
+
+    //Check user input against answer and display results
+    if(getResults(input.value)) {
+        setMessage('You Win! :)');
+        showAnswer(true);
+        showReplay();
+    } else if(attempt.value >= 10) {
+        setMessage('You Lose! :(');
+        showAnswer(false);
+        showReplay();
+    } else {
+        setMessage('Incorrect, try again.');
+    }
+}
+
+function setHiddenFields() {
+  answer.value = Math.floor(Math.random()*9999).toString();
+
+  while(answer.value.length < 4) {
+    answer.value = "0" + answer.value;
+  }
+  attempt.value = 0;
+}
+
+function setMessage(m) {
+  var message = document.getElementById('message');
+  message.innerHTML = m;
+}
+
+function validateInput(input) {
+  if(input.length == 4) {
+    return true;
+  } else {
+    setMessage("Guesses must be exactly 4 characters long.");
+    return false;
+  }
+}
+
+function getResults(input){
+    var correct = 0;
+    var html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+    for(i = 0; i < input.length; i++)
+    {
+        if(input.charAt(i) == answer.value.charAt(i))
+        {
+            html += '<span class="glyphicon glyphicon-ok"></span>';
+            correct++;
+        } else if (answer.value.indexOf(input.charAt(i)) > -1) {
+            html += '<span class="glyphicon glyphicon-transfer"></span>';
+        } else {
+            html += '<span class="glyphicon glyphicon-remove"></span>';
+        }
+    }
+    html += '</div></div>';
+
+    document.getElementById('results').innerHTML += html;
+
+    if(correct == input.length) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+//function shows the answer and colors based on if the player won or lost
+function showAnswer(success){
+    var code = document.getElementById('code');
+    if(success) {
+        code.className += " success";
+    } else {
+        code.className += " failure";
+    }
+    code.innerHTML = answer.value;
+}
+
+function showReplay(){
+    document.getElementById('guessing-div').style.display = "none";
+    document.getElementById('replay-div').style.display = "block";
 }
 
 //impliment new functions here
